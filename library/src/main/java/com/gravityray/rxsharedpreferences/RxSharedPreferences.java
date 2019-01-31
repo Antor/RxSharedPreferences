334d14b814896480531d7a588951de92bbf7c17c
Andrei Tsiatsera <paladin26@narod.ru>
2019-02-01 02:42:57 +0300

    Added methods to RxSharedPreferences for more streamlined access of values in SharedPreferences.

--- library/src/main/java/com/gravityray/rxsharedpreferences/RxSharedPreferences.java
+++ library/src/main/java/com/gravityray/rxsharedpreferences/RxSharedPreferences.java
@@ -5,8 +5,20 @@ import android.support.annotation.NonNull;
 
 import java.util.Set;
 
+import io.reactivex.Observable;
 
+
+@SuppressWarnings({"unused", "WeakerAccess"})
 public class RxSharedPreferences {
+
+    @NonNull
+    public static Observable<String> getStringValue(
+            @NonNull SharedPreferences sharedPreferences,
+            @NonNull String key,
+            String defaultValue) {
+        return getString(sharedPreferences, key, defaultValue)
+                .map(SharedPreferencesValueChangedEvent::getValue);
+    }
 
     @NonNull
     public static SharedPreferencesValueObservable<String> getString(
@@ -18,6 +30,15 @@ public class RxSharedPreferences {
                 key,
                 defaultValue,
                 SharedPreferences::getString);
+    }
+
+    @NonNull
+    public static Observable<Set<String>> getStringSetValue(
+            @NonNull SharedPreferences sharedPreferences,
+            @NonNull String key,
+            Set<String> defaultValue) {
+        return getStringSet(sharedPreferences, key, defaultValue)
+                .map(SharedPreferencesValueChangedEvent::getValue);
     }
 
     @NonNull
@@ -33,6 +54,15 @@ public class RxSharedPreferences {
     }
 
     @NonNull
+    public static Observable<Integer> getIntValue(
+            @NonNull SharedPreferences sharedPreferences,
+            @NonNull String key,
+            int defaultValue) {
+        return getInt(sharedPreferences, key, defaultValue)
+                .map(SharedPreferencesValueChangedEvent::getValue);
+    }
+
+    @NonNull
     public static SharedPreferencesValueObservable<Integer> getInt(
             @NonNull SharedPreferences sharedPreferences,
             @NonNull String key,
@@ -42,6 +72,15 @@ public class RxSharedPreferences {
                 key,
                 defaultValue,
                 SharedPreferences::getInt);
+    }
+
+    @NonNull
+    public static Observable<Long> getLongValue(
+            @NonNull SharedPreferences sharedPreferences,
+            @NonNull String key,
+            long defaultValue) {
+        return getLong(sharedPreferences, key, defaultValue)
+                .map(SharedPreferencesValueChangedEvent::getValue);
     }
 
     @NonNull
@@ -57,6 +96,15 @@ public class RxSharedPreferences {
     }
 
     @NonNull
+    public static Observable<Float> getFloatValue(
+            @NonNull SharedPreferences sharedPreferences,
+            @NonNull String key,
+            float defaultValue) {
+        return getFloat(sharedPreferences, key, defaultValue)
+                .map(SharedPreferencesValueChangedEvent::getValue);
+    }
+
+    @NonNull
     public static SharedPreferencesValueObservable<Float> getFloat(
             @NonNull SharedPreferences sharedPreferences,
             @NonNull String key,
@@ -66,6 +114,15 @@ public class RxSharedPreferences {
                 key,
                 defaultValue,
                 SharedPreferences::getFloat);
+    }
+
+    @NonNull
+    public static Observable<Boolean> getBooleanValue(
+            @NonNull SharedPreferences sharedPreferences,
+            @NonNull String key,
+            boolean defaultValue) {
+        return getBoolean(sharedPreferences, key, defaultValue)
+                .map(SharedPreferencesValueChangedEvent::getValue);
     }
 
     @NonNull
